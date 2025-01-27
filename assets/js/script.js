367be0f965006b664eceea1f17a5e9c64a30e866
Mikamy Vz <wms061097@gmail.com>
2025-01-26 21:42:08 -0300

    #002
    - Rework na seção dúvidas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,22 @@
-// === Menu Mobile === 
-
+// Menu Mobile
 function initMenu() {
-
     const btnMobile = document.getElementById('btn-mobile');
-
     function toggleMenu() {
         const navbar = document.getElementById('navbar');
         navbar.classList.toggle('active');
     }
-
     btnMobile.addEventListener('click', toggleMenu);
-
 }
 initMenu();
+
+// FAQ
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+        const answer = button.nextElementSibling;
+        if (answer.style.display === 'flex') {
+            answer.style.display = 'none';
+        } else {
+            answer.style.display = 'flex';
+        }
+    });
+});
